a7c5724588ec40b9d0c24bce3702a1aca52bd5d3
Prakshal Jain <prakshaljain927@gmail.com>
2026-02-19 03:34:23 +0530

    Hardcoded Render backend URL

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "https://national-digital-bank-backend.onrender.com/api",
+  baseURL: "https://nationaldigitalbank-0ufr.onrender.com/",
   headers: {
     "Content-Type": "application/json",
   },
